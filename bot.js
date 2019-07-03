295699f285e16f33f6260eff4a9e82315d635ba9
jcraggie <jcraggie@hotmail.com>
2019-07-03 06:41:27 -0500

    2019-07-03 revised code

--- bot.js
+++ bot.js
@@ -12,4 +12,4 @@ client.on('message', message => {
 });
  
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.mc8J4dvekK0MV2UWE0KYh32qs9ZWQEBV);
+client.login(process.env.BOT_TOKEN); //BOT_TOKEN is the Client Secret
