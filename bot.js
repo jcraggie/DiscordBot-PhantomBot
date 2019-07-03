d1a56c413855baee6185cc68028682ad5a92911a
jcraggie <jcraggie@hotmail.com>
2019-07-03 08:24:23 -0500

    2019-07-03 update for command prefix !

--- bot.js
+++ bot.js
@@ -10,6 +10,33 @@ client.on('message', message => {
        message.reply('pong');
        }
 });
+
+client.on('message', function (user, userID, channelID, message, evt) {
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `!`
+    if (message.substring(0, 1) == '!') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+       
+        args = args.splice(1);
+        switch(cmd) {
+            // !ping
+            case 'ping':
+                client.sendMessage({
+                    to: channelID,
+                    message: 'Pong!'
+                });
+			case 'pong':
+                client.sendMessage({
+                    to: channelID,
+                    message: 'Ping!'
+                });
+
+            break;
+            // Just add any case commands if you want to..
+         }
+     }
+});
  
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN); //BOT_TOKEN is the Client Secret
