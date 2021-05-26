7b95d849ebe4a8b5af51b3b5828d160614320857
jcraggie <jcraggie@hotmail.com>
2021-05-26 17:21:06 -0500

    updating Havoc and adding Rogue cron ticket reminders

--- events/client/ready.js
+++ events/client/ready.js
@@ -7,7 +7,9 @@ module.exports = () =>{
 // 586291147169857556 PhantomHavoc role (members)
 
 var CronJob = require('cron').CronJob;
-var havocTime = '0 30 18 * * *'
+
+
+var havocTime = '0 30 17 * * *'
 var pHavTz = 'America/Chicago';
 
 var cronHavoc = new CronJob(havocTime, function() {
@@ -36,9 +38,39 @@ let havocTicketReminder = globalVar.phantomBotHelp
 
     }, null, true, pHavTz); // central
 
-    cronHavoc.start();
+    cronHavoc.start();  //end cronHavoc
 
+    // 581166616872747018 Phantom Rogue role ID (members)
+    // 604386931178209310 PhantomRogue #ticket-warnings-ro channel
+    var rogueTime = '0 30 19 * * *'; //ticket time is 20:30 CST, so reminder should be 19:30
+    var pRogTz = 'America/Chicago';
+    var cronRogue = new CronJob(rogueTime, function() {
 
+        console.log('sending Rogue ticket reminder.');
+        
+        let ticketImage = new Discord.MessageAttachment('./graphics/TicketReminder01.png');
+        
+        var globalVar = require('../../global.js');
+        let rogueTicketReminder = globalVar.phantomBotHelp
+        
+                .setTitle('phantomROGUE 600 TICKET REMINDER!')
+                .attachFiles(ticketImage)
+                .setImage('attachment://TicketReminder01.png')
+                .setDescription("<@&581166616872747018> Tickets are due in 1 hour!")
+        
+                //send message to Havoc lounge
+                client.channels.cache.get('604386931178209310').send(rogueTicketReminder);
+        
+        
+                //log the event to jcrAggie server #phantom-ready channel
+                client.channels.cache.get('605087450573963362').send("Bot issued cronRogue reminder.");
+        
+                //log the event to the console
+                console.log('Bot sent cronRogue'); 
+        
+            }, null, true, pRogTz); // central
+        
+            cronRogue.start();  //end cronHavoc
 
 
 
