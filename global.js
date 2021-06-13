995c022c833e5b40e13be381d5fc8fcffcd4da62
jcraggie <jcraggie@hotmail.com>
2021-06-13 02:54:36 -0500

    version 3.0.1 - adding guild json files; serverstats cmd and filehelper.js; preparing for file read/write

--- global.js
+++ global.js
@@ -1,14 +1,32 @@
-var footer = 'PhantomBot made by jcrAggie for the PhantomAlliance. \nThis is the way.   Ver 3.0 beta';
+var footer = 'PhantomBot made by jcrAggie for the PhantomAlliance. \nThis is the way.   Ver 3.0.1 ';
 exports.footer = footer
 
-let Discord = require('discord.js');
+const discord_jcrAggie = "116901947428044809";
+exports.discord_jcrAggie = discord_jcrAggie
 
+const discord_jcraggie93 = "419494570213244939";
+exports.discord_jcraggie93 = discord_jcraggie93
+
+const not_jcr_msg = "Sorry. You are not jcrAggie or NOTjcraggie.\n**I have spoken.**"
+exports.not_jcr_msg = not_jcr_msg
+
+const not_auth_msg = "You are not authorized to use this command.\n**I have spoken.**"
+exports.not_auth_msg = not_auth_msg
+
+const cmd_being_tested = "Sorry. This command is being tested by jcrAggie.\n**I have spoken.**"
+exports.cmd_being_tested = cmd_being_tested
+
+
+
+
+let Discord = require('discord.js');
 const phantomBotHelp = new Discord.MessageEmbed()
         .setTitle("PhantomBot Help")
         .setColor(0x580202)
         .setTimestamp()
         .setFooter(footer);
-
+        
+        phantomBotHelp.fields = [];
 
 exports.phantomBotHelp = phantomBotHelp
 
@@ -27,6 +45,7 @@ const fancyPitImageEmbed = new Discord.MessageEmbed()
         .setColor(0x580202)
         .setTimestamp()
         .setFooter(footer);
+
 
 
 exports.fancyPitImageEmbed = fancyPitImageEmbed
