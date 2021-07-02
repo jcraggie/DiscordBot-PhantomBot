7a17d73627887c65eeb664a66b256765836f5ea3
jcraggie <jcraggie@hotmail.com>
2021-07-02 14:40:43 -0500

    debugging again

--- commands/invite.js
+++ commands/invite.js
@@ -32,7 +32,7 @@ module.exports = {
         } else {
 
             //log the event to Discord (jcrAggie server) and the console
-            console.log('---MESSAGE: ', message);
+            console.log('---MESSAGE: ', message.content);
             console.log('--- PB.INVITE ARGS: ', args);
             fileUtils.logToDiscordAndConsole(client, message, args, Discord);
 
