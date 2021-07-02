ce472bb3d849b4db51152463c4a3a96aa977ead6
jcraggie <jcraggie@hotmail.com>
2021-07-01 22:05:35 -0500

    imported newcommandshell; created currepoch cmd

---
+++ commands/currepoch.js
@@ -0,0 +1,23 @@
+module.exports = {
+    name: 'currepoch',
+    description: "description of the new command",
+    async execute(client, message, args, Discord,swapi, ApiSwgohHelp){
+        const globalVar = require ('../global.js');
+        var fileUtils = require('../fileHelper.js');
+        // message.channel.send('currepoch');
+
+        var currEpoch = Math.floor(new Date().getTime()/1000.0)
+        console.log('---current time test', currEpoch)
+
+        var fetchDateConvert = new Date(currEpoch + 1000); //convert epoch timestamp to date and time
+        var fetchLocalDate = fetchDateConvert.toLocaleString(); //convert date and time to local
+
+        console.log('---current time Text: ', fetchLocalDate);
+        
+        //log the event to Discord (jcrAggie server) and the console
+        fileUtils.logToDiscordAndConsole(client, message, args, Discord);
+
+
+    }//end async execute
+
+}//end module.exports
