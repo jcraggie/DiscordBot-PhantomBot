a71206bebaa13cda20b06ebba4ed72e8cead73b6
jcraggie <jcraggie@hotmail.com>
2021-07-01 23:44:26 -0500

    try this

--- commands/mongoupdateall.js
+++ commands/mongoupdateall.js
@@ -61,7 +61,7 @@ module.exports = {
         async function updateDB(apiGuildData) {
             for(const gData of apiGuildData) {
                 //console.log('---GDATA: \n', gData)
-                var currEpoch = Math.floor(new Date().getTime())
+                var currEpoch = Math.floor(new Date().getTime()/1000)
                 currEpoch += -18000;
                 console.log('-------UPDATING MONGODB FOR: ', gData.name)
                 console.log('---fetch time epoch', currEpoch)
