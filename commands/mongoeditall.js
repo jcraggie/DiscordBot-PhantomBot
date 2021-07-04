56602e453ff4ae316d691bc906f4ddbde7699e29
jcraggie <jcraggie@hotmail.com>
2021-07-04 10:26:44 -0500

    chg for...in back to for...of

--- commands/mongoeditall.js
+++ commands/mongoeditall.js
@@ -385,7 +385,7 @@ module.exports = {
                         console.log('---MONGOOSE CONNECTION IS NOW CLOSED');
                       });
                     gld = '';
-                    for(gld in gNames) {
+                    for(gld of gNames) {
                         // message.channel.send(guildUpdateEmbed[gld]);
                         console.log('---SENDING EMBED FOR GUILD: ' + gld);
                         // message.channel.send('Sending embed for: `'+ gld + '`');
