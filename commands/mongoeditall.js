b9a6d52462ee287495f4012a296af4c0456b474b
jcraggie <jcraggie@hotmail.com>
2021-07-04 12:35:11 -0500

    closing in on the bug...

--- commands/mongoeditall.js
+++ commands/mongoeditall.js
@@ -375,7 +375,7 @@ module.exports = {
 
                 // console.log('---SAVING EMBED FOR ' + gld);
                 guildUpdateEmbed[gld] = guildEmbed; //dynamic var guildUpdateEmbed[rebellion] etc.
-                console.log('---THIS IS guildUpdateEmbed[gld]: \n', guildUpdateEmbed[gld]);
+                console.log('---THIS IS guildUpdateEmbed[rebellion]: \n', guildUpdateEmbed['rebellion']);
                 // console.log('---THIS IS THE SAVED EMBED FOR ' + gld + ':\n',guildUpdateEmbed[gld]);
 
                 
