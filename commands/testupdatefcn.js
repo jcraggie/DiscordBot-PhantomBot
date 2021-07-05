18e9d037de36d9db63cd2871d11097a8489c5768
jcraggie <jcraggie@hotmail.com>
2021-07-04 23:12:31 -0500

    testing new fcn

---
+++ commands/testupdatefcn.js
@@ -0,0 +1,17 @@
+module.exports = {
+    name: 'testupdatefcn',
+    description: "description of the new command",
+    async execute(client, message, args, Discord,swapi, ApiSwgohHelp){
+        const globalVar = require ('../global.js');
+        var fileUtils = require('../fileHelper.js');
+        var fcn = require('./fcnsendupdates.js');
+        // message.channel.send('testupdatefcn');
+        fcn.sendGuildUpdates(client, message, args, Discord,swapi, ApiSwgohHelp);
+        
+        //log the event to Discord (jcrAggie server) and the console
+        fileUtils.logToDiscordAndConsole(client, message, args, Discord);
+
+
+    }//end async execute
+
+}//end module.exports
